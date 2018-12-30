c44e5ea52d105d9a1ab9753f3ab60d517c069ffb
duanyll <xiaoguo2004@sina.cn>
2018-12-30 15:53:57 +0800

    重大更新!现在支持画图片了

--- lgpb/Program.cs
+++ lgpb/Program.cs
@@ -2,6 +2,10 @@
 using System.IO;
 using System.Collections.Generic;
 using System.Threading;
+using System.Drawing;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using System.Text.RegularExpressions;
 
 namespace lgpb
 {
@@ -13,7 +17,8 @@ namespace lgpb
         static int YStart = 0;
         static string[] TextToWrite;
         private static object Locker = new object();
-        static bool MoveNext(){
+        static bool MoveNext()
+        {
             NowX++;
             if (NowX >= TextToWrite[NowY].Length)
             {
@@ -28,11 +33,143 @@ namespace lgpb
             return true;
         }
 
-        static bool Draw(AJAXCrawler crawler,int X,int Y,int Color){
+        static bool MoveNext(int w,int h)
+        {
+            NowX++;
+            if (NowX >= w)
+            {
+                NowY++;
+                NowX = 0;
+                if (NowY >= h)
+                {
+                    Console.WriteLine("绘制完成");
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        static string[] ColorList = {
+            "rgb(0, 0, 0)", "rgb(255, 255, 255)", "rgb(170, 170, 170)", "rgb(85, 85, 85)", "rgb(254, 211, 199)", "rgb(255, 196, 206)", "rgb(250, 172, 142)", "rgb(255, 139, 131)", "rgb(244, 67, 54)", "rgb(233, 30, 99)", "rgb(226, 102, 158)", "rgb(156, 39, 176)", "rgb(103, 58, 183)", "rgb(63, 81, 181)", "rgb(0, 70, 112)", "rgb(5, 113, 151)", "rgb(33, 150, 243)", "rgb(0, 188, 212)", "rgb(59, 229, 219)", "rgb(151, 253, 220)", "rgb(22, 115, 0)", "rgb(55, 169, 60)", "rgb(137, 230, 66)", "rgb(215, 255, 7)", "rgb(255, 246, 209)", "rgb(248, 203, 140)", "rgb(255, 235, 59)", "rgb(255, 193, 7)", "rgb(255, 152, 0)", "rgb(255, 87, 34)", "rgb(184, 63, 39)", "rgb(121, 85, 72)"
+        };
+        static List<Rgba32> Colors = new List<Rgba32>();
+
+        static Rgba32 LoadColorFromString(string str)
+        {
+            var vs = str.Substring(4, str.Length - 5).Replace(" ", "").Split(',');
+            return new Rgba32(float.Parse(vs[0])/256, float.Parse(vs[1])/256, float.Parse(vs[2])/256);
+        }
+
+        static int GetColorID(Rgba32 col)
+        {
+            int mnval = int.MaxValue, mxpos = 0;
+            for (int i = 0; i < Colors.Count; i++)
+            {
+                int dis = Math.Abs(Colors[i].R - col.R) + Math.Abs(Colors[i].G - col.G) + Math.Abs(Colors[i].B - col.B);
+                if (dis < mnval)
+                {
+                    mxpos = i;
+                    mnval = dis;
+                }
+            }
+            return mxpos;
+        }
+
+        static bool Draw(AJAXCrawler crawler, int X, int Y, int Color)
+        {
             crawler.Content = $"x={X}&y={Y}&color={Color}";
             string result = crawler.PostForm();
             System.Console.WriteLine(result);
             return result.Contains("200");
+            // System.Console.WriteLine($"{X},{Y},{Color}");
+            // return true;
+        }
+
+        static void WriteTextBackGround(List<AJAXCrawler> crawlers)
+        {
+            TextToWrite = File.ReadAllLines("data");
+            Console.WriteLine("操作开始");
+            foreach (var i in crawlers)
+            {
+                Thread th = new Thread(() =>
+                {
+                    while (NowY < TextToWrite.Length)
+                    {
+                        Thread.Sleep(0);
+                        lock (Locker)
+                        {
+                            while (TextToWrite[NowY][NowX] == ' ')
+                            {
+                                if (!MoveNext())
+                                {
+                                    System.Console.WriteLine("操作完成");
+                                    return;
+                                }
+                            }
+
+                            if (Draw(i, XStart + NowX, YStart + NowY, 9))
+                            {
+                                Console.WriteLine($"已绘制{NowX},{NowY}");
+                            }
+                            else
+                            {
+                                Thread.Sleep(1000);
+                                continue;
+                            }
+
+                            if (!MoveNext())
+                            {
+                                System.Console.WriteLine("操作完成");
+                                return;
+                            }
+                        }
+                        Thread.Sleep(32000);
+                    }
+                    Console.WriteLine("操作完成");
+                });
+                th.Start();
+            }
+        }
+
+        static void DrawImageBackGround(List<AJAXCrawler> crawlers)
+        {
+            Image<Rgba32> img = Image.Load(File.ReadAllBytes("image.png"));
+            foreach (var i in ColorList)
+            {
+                Colors.Add(LoadColorFromString(i));
+            }
+            Console.WriteLine("操作开始");
+            foreach (var i in crawlers)
+            {
+                Thread th = new Thread(() =>
+                {
+                    while (NowY < img.Height)
+                    {
+                        Thread.Sleep(0);
+                        lock (Locker)
+                        {
+                            if (Draw(i, XStart + NowX, YStart + NowY, GetColorID(img[XStart + NowX,YStart + NowY])))
+                            {
+                                Console.WriteLine($"已绘制{NowX},{NowY}");
+                            }
+                            else
+                            {
+                                Thread.Sleep(1000);
+                                continue;
+                            }
+
+                            if (!MoveNext(img.Width,img.Height))
+                            {
+                                System.Console.WriteLine("操作完成");
+                                return;
+                            }
+                        }
+                        Thread.Sleep(32000);
+                    }
+                    Console.WriteLine("操作完成");
+                });
+                th.Start();
+            }
         }
         static void Main(string[] args)
         {
@@ -53,43 +190,10 @@ namespace lgpb
                 NowX = int.Parse(args[2].Trim());
                 NowY = int.Parse(args[3].Trim());
             }
-            TextToWrite = File.ReadAllLines("data");
-            Console.WriteLine("操作开始");
-            foreach (var i in crawlers)
-            {
-                Thread th = new Thread(() =>
-                {
-                    while (NowY < TextToWrite.Length)
-                    {
-                        Thread.Sleep(0);
-                        lock (Locker)
-                        {
-                            while (TextToWrite[NowY][NowX] == ' ')
-                            {
-                                if(!MoveNext()){
-                                    System.Console.WriteLine("操作完成");
-                                    return;
-                                }
-                            }
 
-                            if(Draw(i,XStart+NowX,YStart+NowY,9)){
-                                Console.WriteLine($"已绘制{NowX},{NowY}");
-                            }else{
-                                Thread.Sleep(1000);
-                                continue;
-                            }                            
+            //WriteTextBackGround(crawlers);
+            DrawImageBackGround(crawlers);
 
-                            if(!MoveNext()){
-                                System.Console.WriteLine("操作完成");
-                                return;
-                            }
-                        }
-                        Thread.Sleep(32000);
-                    }
-                    Console.WriteLine("操作完成");
-                });
-                th.Start();
-            }
             while (true)
             {
                 Console.ReadKey();
